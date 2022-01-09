5b276852eec5e6e7a6ebead8930db92e2ce78b94
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-08 22:50:02 -0500

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+        <img
+          className="navbar-logo"
+          alt="drawit logo"
+          src="/drawit_logo.png"
+        />
+    </nav>
+  );
+}
