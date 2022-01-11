c0efb0bd06725d05cef566925837735d5f5f696a
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-10 19:40:26 -0500

    Add content in Landing Page component and browserRouter in index.js

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LandingPage() {
+  return (
+    <nav>
+        <img
+          className="w-52"
+          alt="drawit logo"
+          src="/drawit_logo.png"
+        />
+    </nav>
+  );
+}
