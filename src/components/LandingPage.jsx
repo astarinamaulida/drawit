a956e9e2b0be6f112675ab013efa184a90606ad4
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-10 20:58:18 -0500

    Fix the jsx structure

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,14 +2,8 @@ import React from "react";
 
 export default function LandingPage() {
   return (
-    <nav>
-      <div className="flex items-center justify-center h-screen">
-        <img
-          className="w-80"
-          alt="drawit logo"
-          src="/drawit_logo.png"
-        />
-        </div>
-    </nav>
+    <div className="flex items-center justify-center h-screen">
+      <img className="w-80" alt="drawit logo" src="/drawit_logo.png" />
+    </div>
   );
 }
