f70531c8afe6dca84c03e11b9cf95848ce25098e
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-10 20:53:31 -0500

    Add flex to place the logo position

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function LandingPage() {
   return (
     <nav>
+      <div className="flex items-center justify-center h-screen">
         <img
           className="w-80"
           alt="drawit logo"
           src="/drawit_logo.png"
         />
+        </div>
     </nav>
   );
 }
