c48a9e9188220a980372cdc0e4e96d508b911898
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-11 00:17:12 -0500

    Add annimation

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
     <div>
       <div className="fixed h-full w-full flex items-center justify-center bg-bubble-gum">
         <Link to="/draw">
-          <img className="w-80 p-1 bg-white" alt="drawit logo" src="/landingpage_logo.png" />
+          <img className="w-80 animate-bounce" alt="drawit logo" src="/landingpage_logo.png" />
         </Link>
       </div>
     </div>
