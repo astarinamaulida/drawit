6a18a2cfa4c33b2b47ac1f95ceec0071af5d9afa
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-10 23:59:20 -0500

    Change bubble gum color as background

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default function LandingPage() {
   return (
     <div>
-      <div className="fixed h-full w-full flex items-center justify-center bg-cyan-400">
+      <div className="fixed h-full w-full flex items-center justify-center bg-bubble-gum">
         <Link to="/draw">
-          <img className="w-80" alt="drawit logo" src="/drawit_logo.png" />
+          <img className="w-80 p-1 bg-white" alt="drawit logo" src="/drawit_logo.png" />
         </Link>
       </div>
     </div>
