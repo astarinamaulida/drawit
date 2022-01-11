7db34c0c25e66b5dd8470af49013242723db9de6
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-10 20:02:49 -0500

    Change logo size

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
   return (
     <nav>
         <img
-          className="w-52"
+          className="w-80"
           alt="drawit logo"
           src="/drawit_logo.png"
         />
