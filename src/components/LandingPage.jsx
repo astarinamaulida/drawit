180a27dc9fc1316840c695ccdd4b7a52bec389cc
Astarina Maulida <astarina.maulida@gmail.com>
2022-01-10 21:30:40 -0500

    Add link when clicking the logo

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
   return (
     <div>
       <div className="fixed h-full w-full flex items-center justify-center bg-cyan-400">
-        <img className="w-80" alt="drawit logo" src="/drawit_logo.png" />
+        <Link to="/draw">
+          <img className="w-80" alt="drawit logo" src="/drawit_logo.png" />
+        </Link>
       </div>
     </div>
   );
